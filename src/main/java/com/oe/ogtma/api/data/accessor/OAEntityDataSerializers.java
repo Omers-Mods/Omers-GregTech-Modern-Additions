9732bdcdb75bf3a3e90d33c7e1baa7846fbbaa04
omergunr100 <omer.er10@gmail.com>
2025-02-01 20:12:18 +0200

    add serializer for vec3 just in case

--- src/main/java/com/oe/ogtma/api/data/accessor/OAEntityDataSerializers.java
+++ src/main/java/com/oe/ogtma/api/data/accessor/OAEntityDataSerializers.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
 public class OAEntityDataSerializers {
 
@@ -30,9 +31,15 @@ public class OAEntityDataSerializers {
                 }
                 return arr;
             });
+    public static final EntityDataSerializer<Vec3> VEC3 = EntityDataSerializer.simple((buf, v) -> {
+        buf.writeDouble(v.x);
+        buf.writeDouble(v.y);
+        buf.writeDouble(v.z);
+    }, buf -> new Vec3(buf.readDouble(), buf.readDouble(), buf.readDouble()));
 
     public static void init() {
         EntityDataSerializers.registerSerializer(AABB);
         EntityDataSerializers.registerSerializer(BLOCK_POS_ARRAY);
+        EntityDataSerializers.registerSerializer(VEC3);
     }
 }
